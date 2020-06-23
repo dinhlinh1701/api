7990db61020ae6a62cb08d3794b002893d638b84
dinhlinh1701 <dinhlinh1701@gmail.com>
2020-06-23 22:23:40 +0700

    gg

---
+++ webAPI/webAPI/Migrations/20200622075812_a.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webAPI.Migrations
+{
+    public partial class a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankAccounts",
+                columns: table => new
+                {
+                    AccountID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    AccountHolder = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    BankID = table.Column<string>(nullable: false),
+                    IFSC = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankAccounts", x => x.AccountID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    BankID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BankName = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.BankID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+        }
+    }
+}
---
+++ webAPI/webAPI/Models/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAPI.Models
+{
+    public class BankAccount
+    {   
+        [Key]
+        public int AccountID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string AccountHolder { get; set; }
+        [Required]
+        public string BankID { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string IFSC { get; set; }
+
+    }
+}
